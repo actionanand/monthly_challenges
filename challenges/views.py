a818715843c805b1b94c667c998bd2c80df7743d
Anand raja <meetanandns@gmail.com>
2022-02-08 16:32:23 +0000

    htlm view

--- challenges/views.py
+++ challenges/views.py
@@ -24,16 +24,17 @@ monthly_challenges = {
 def monthly_challenge(request, month):
     try:
         challenge_text = monthly_challenges[month]
-        return HttpResponse(challenge_text)
+        response_data = f"<h1>{challenge_text}</h1>"
+        return HttpResponse(response_data)
     except:
-        return HttpResponseNotFound('This month is not supported!')
+        return HttpResponseNotFound('<h1>This month is not supported!</h1>')
 
 
 def monthly_challenge_by_number(request, month):
     months = list(monthly_challenges.keys())
 
     if month > len(months):
-        return HttpResponseNotFound('Invalid Month')
+        return HttpResponseNotFound('<h1>Invalid Month</h1>')
 
     redirect_month = months[month - 1]
     redirect_path = reverse('month-challenge', args=[redirect_month])   #  '/challenges/january'
