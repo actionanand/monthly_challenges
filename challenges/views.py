08eed939868e8fbe8813fb5819d99013cb202538
Anand raja <meetanandns@gmail.com>
2022-02-08 17:15:57 +0000

    rendering html

--- challenges/views.py
+++ challenges/views.py
@@ -37,8 +37,9 @@ def index(request):
 def monthly_challenge(request, month):
     try:
         challenge_text = monthly_challenges[month]
-        response_data = render_to_string('challenges/challenge.html')
-        return HttpResponse(response_data)
+        return render(request, 'challenges/challenge.html')
+        # response_data = render_to_string('challenges/challenge.html')
+        # return HttpResponse(response_data)
     except:
         return HttpResponseNotFound('<h1>This month is not supported!</h1>')
 
