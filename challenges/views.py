2f05c8dc9fbe8267ea688679e468a6926b5b0a87
Anand raja <meetanandns@gmail.com>
2022-02-08 17:21:49 +0000

    using temp var in html

--- challenges/views.py
+++ challenges/views.py
@@ -37,7 +37,9 @@ def index(request):
 def monthly_challenge(request, month):
     try:
         challenge_text = monthly_challenges[month]
-        return render(request, 'challenges/challenge.html')
+        return render(request, 'challenges/challenge.html', {
+            'text': challenge_text
+        })
         # response_data = render_to_string('challenges/challenge.html')
         # return HttpResponse(response_data)
     except:
